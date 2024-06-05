70fd1d6bd77e3e6c4149e8c209f57b76a235fe31
janjao937 <janjao937@gmail.com>
2024-06-05 16:06:50 +0700

    UI design frontend

---
+++ frontend/src/component/sidebar/SearchInput.jsx
@@ -0,0 +1,16 @@
+import { RiUserSearchLine } from "react-icons/ri";
+
+const SearchInput=()=>{
+    return (
+        <form className="flex items-center gap-2">
+            <input type="text" placeholder="Search..." className="input input-bordered rounded-full"></input>
+
+            <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+                <RiUserSearchLine className="w-6 h-6 outline-none" />
+            </button>
+        
+        </form>
+    );
+}
+
+export default SearchInput;
