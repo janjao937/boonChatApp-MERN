998ff100070796a19a2489cc8761360b9e4bb466
janjao937 <janjao937@gmail.com>
2024-07-02 22:07:14 +0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // const express = require("express");
 // const dotenv = require("dotenv");
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectToMongoDb from "./db/connectToMongoDB.js";
@@ -14,6 +15,8 @@ dotenv.config();
 
 const PORT = process.env.PORT||5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());//json payload from req.body
 app.use(cookieParser());//parse data in cookie from req
 
@@ -21,10 +24,12 @@ app.use("/api/auth",authRoute);
 app.use("/api/message",messageRoute);
 app.use("/api/users",userRoute);
 
+//https://expressjs.com/en/starter/static-files.html
+app.use(express.static(path.join(__dirname,"/frontend/dist")));//share file(img,sound,video,etc.) in Folder between frontend and backend //dist จะถูกสร้างตอน build ใน frontend
 
-
-
-
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));//middleware to frontend file
+})
 
 
 server.listen(PORT,()=>{
